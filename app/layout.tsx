12f92d707d719d82d06b22088e25fb6131d87111
shikha22 <shikha2022r2@gmail.com>
2025-01-18 21:41:37 +0530

    navbar and sidebar done

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.className} bg-dark-2`}>{children}
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,13 @@ export default {
   theme: {
   	extend: {
   		colors: {
+			dark:{
+				1:'#1C1F2E',
+				2:'#161925',
+			},
+			blue:{
+				1:'#0E78F9'
+			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
